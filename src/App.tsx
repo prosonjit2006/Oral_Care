ed59ba995d2e3d8bc1114ef1d7acd3abd4dd3b0c
prosonjit2006 <prosonjitdatta2006@gmail.com>
2026-03-02 14:37:21 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
